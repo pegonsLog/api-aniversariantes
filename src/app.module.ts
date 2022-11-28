14435c040ce344a4f2e2e8950b5ca93fa941cde4
pegonsLog <pegons.log@gmail.com>
2022-11-28 12:08:40 -0300

    28/11-trabalho

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,20 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BirthdaysModule } from './birthdays/birthdays.module';
 import { Birthday } from './birthdays/entities/birthday.entity';
+import { User } from './users/entities/user.entity';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       database: './db.sql',
       type: 'sqlite',
-      entities: [Birthday],
+      entities: [Birthday, User],
       synchronize: true,
+      logging: true,
     }),
     BirthdaysModule,
+    UsersModule,
   ],
   controllers: [],
   providers: [],
---
+++ src/users/users.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  create(createUserDto: CreateUserDto) {
+    const newUser = this.usersRepository.create(createUserDto);
+    return this.usersRepository.save(newUser);
+  }
+
+  async findAll() {
+    return await this.usersRepository.find();
+  }
+
+  async findOne(id: number) {
+    return await this.usersRepository.findOneBy({ id: id });
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    return await this.usersRepository.update(id, updateUserDto);
+  }
+
+  remove(id: number) {
+    return this.usersRepository.delete(id);
+  }
+}
